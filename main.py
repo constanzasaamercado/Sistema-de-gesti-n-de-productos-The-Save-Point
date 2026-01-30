03f4ba83245490aaa0d62abe54d0de58d76dc931
Constanza Saa <constanza.saa.mercado@gmail.com>
2026-01-29 22:26:53 -0300

    Saludo de inicio del sistema borrador 1

---
+++ main.py
@@ -0,0 +1,10 @@
+# Encabezado
+
+print("*******************************")
+print("* BIENVENIDO A THE SAVE POINT *")
+print("*******************************")
+
+# Ingreso de usuario
+
+nombre_usuario = input("Por favor, ingresa tu nombre de usuario: ")
+print(f"¡Hola, {nombre_usuario}! iniciando sistema de gestión de productos.")
