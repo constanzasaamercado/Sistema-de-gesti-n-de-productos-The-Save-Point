cbae51aedcb93d581b67b9104b1c0c9c5d0445c1
Constanza Saa <constanza.saa.mercado@gmail.com>
2026-01-31 00:12:37 -0300

    Se cambia el diccionario por una lista de diccionario para que funcione el sistema de visualizacion. Se arregla el codigo de la funcion de visualizacion para adaptarla a los cambios realizados.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Importar módulo de gestión de datos
 import modulos.gestion_datos as gestion
-from modulos.gestion_datos import capturar_datos, mostrar_inventario, eliminar_videojuego
+from modulos.gestion_datos import capturar_datos, mostrar_inventario, eliminar_videojuego, videojuegos_inventario
+
 
 # Encabezado
 
@@ -14,14 +15,16 @@ nombre_usuario = input("Por favor, ingresa tu nombre de usuario: ")
 print(f"¡Hola, {nombre_usuario}! iniciando sistema de gestión de productos.")
 
 
-videojuegos_inventario = []
+# videojuegos_inventario = []
 
-
+# Menu del sistema de gestión
 
 while True:
         print("\n--- GESTIÓN DE PRODUCTOS---")
-        print("1. Ingresar datos")
-        print("2. Visualización de inventario")
+        print("1. Ingresar datos de nuevo videojuego")
+        print("2. Modificar datos de videojuego")
+        print("3. Eliminar videojuego")
+        print("4. Visualización de inventario")
         print("5. Salir")
 
         opcion = input("\nSeleccione una opción: ")
@@ -30,12 +33,17 @@ while True:
             nuevo_videojuego = capturar_datos()
             videojuegos_inventario.append(nuevo_videojuego)
             print(f"El videojuego {nuevo_videojuego['Titulo']} ha sido guardado!")
-
-        elif opcion == "2":
-            mostrar_inventario(videojuegos_inventario)
         
+        elif opcion == "2":
+            print("Funcionalidad de modificación de datos aún no implementada.")
+
         elif opcion == "3":
             eliminar_videojuego(videojuegos_inventario)
+
+
+        elif opcion == "4":
+            mostrar_inventario(videojuegos_inventario)
+        
         elif opcion == "5":
             print(f"¡Hasta luego {nombre_usuario}! Cerrando The Save Point...")
             break
--- modulos/gestion_datos.py
+++ modulos/gestion_datos.py
@@ -1,19 +1,19 @@
 # Diccionario para almacenar los datos de los productos
 
-videojuegos_inventario ={
-    "001": {"Titulo": "Super Mario Bros.", "Genero": "Plataformas", "Anio":"1985", "Plataforma": "NES", "Editor": "Nintendo", "Formato": "Físico", "Precio": 15.000, "Stock": 5},
-    "002": {"Titulo": "Mario Kart Wii", "Genero": "Carreras", "Anio":"2008", "Plataforma": "Wii", "Editor": "Nintendo", "Formato": "Físico", "Precio": 25.000, "Stock": 3},
-    "003": {"Titulo": "Wii Sports Resort", "Genero": "Deportes", "Anio":"2009", "Plataforma": "Wii", "Editor": "Nintendo", "Formato": "Físico", "Precio": 20.000, "Stock": 2},
-    "004": {"Titulo": "Detective Pikachu", "Genero": "Aventura", "Anio":"2019", "Plataforma": "Switch", "Editor": "Nintendo", "Formato": "Digital", "Precio": 35.000, "Stock": 4},
-    "005": {"Titulo": "Mortal kombat 11", "Genero": "Lucha", "Anio":"2019", "Plataforma": "Pc", "Editor": "NetherRealm Studios", "Formato": "Digital", "Precio": 35.000, "Stock": 15},
-    "006": {"Titulo": "Alice Madness Returns", "Genero": "Acción", "Anio":"2011", "Plataforma": "Pc", "Editor": "Spicy Horse", "Formato": "Digital", "Precio": 10.000, "Stock": 8},
-    "007": {"Titulo": "The Witcher 3: Wild Hunt", "Genero": "RPG", "Anio":"2015", "Plataforma": "Pc", "Editor": "CD Projekt", "Formato": "Digital", "Precio": 40.000, "Stock": 12},
-    "008": {"Titulo": "Let's go Pikachu", "Genero": "Aventura", "Anio":"2018", "Plataforma": "Switch", "Editor": "Nintendo", "Formato": "Físico", "Precio": 30.000, "Stock": 15},
-    "009": {"Titulo": "Let's go Eevee", "Genero": "Aventura", "Anio":"2018", "Plataforma": "Switch", "Editor": "Nintendo", "Formato": "Físico", "Precio": 30.000, "Stock": 10},
-    "010": {"Titulo": "Untitled Goose Game", "Genero": "Aventura", "Anio":"2019", "Plataforma": "Pc", "Editor": "House House", "Formato": "Digital", "Precio": 15.000, "Stock": 20},
-    "011": {"Titulo": "God of War", "Genero": "Acción", "Anio":"2018", "Plataforma": "Pc", "Editor": "Santa Monica Studio", "Formato": "Digital", "Precio": 45.000, "Stock": 7},
-    "012": {"Titulo": "Mario kart 8 Deluxe", "Genero": "Carreras", "Anio":"2017", "Plataforma": "Switch", "Editor": "Nintendo", "Formato": "Físico", "Precio": 40.000, "Stock": 9}, 
-}
+videojuegos_inventario =[
+    {"Titulo": "Super Mario Bros.", "Genero": "Plataformas", "Anio":"1985", "Plataforma": "NES", "Editor": "Nintendo", "Formato": "Físico", "Precio": 15.000, "Stock": 5},
+    {"Titulo": "Mario Kart Wii", "Genero": "Carreras", "Anio":"2008", "Plataforma": "Wii", "Editor": "Nintendo", "Formato": "Físico", "Precio": 25.000, "Stock": 3},
+    {"Titulo": "Wii Sports Resort", "Genero": "Deportes", "Anio":"2009", "Plataforma": "Wii", "Editor": "Nintendo", "Formato": "Físico", "Precio": 20.000, "Stock": 2},
+    {"Titulo": "Detective Pikachu", "Genero": "Aventura", "Anio":"2019", "Plataforma": "Switch", "Editor": "Nintendo", "Formato": "Digital", "Precio": 35.000, "Stock": 4},
+    {"Titulo": "Mortal kombat 11", "Genero": "Lucha", "Anio":"2019", "Plataforma": "Pc", "Editor": "NetherRealm Studios", "Formato": "Digital", "Precio": 35.000, "Stock": 15},
+    {"Titulo": "Alice Madness Returns", "Genero": "Acción", "Anio":"2011", "Plataforma": "Pc", "Editor": "Spicy Horse", "Formato": "Digital", "Precio": 10.000, "Stock": 8},
+    {"Titulo": "The Witcher 3: Wild Hunt", "Genero": "RPG", "Anio":"2015", "Plataforma": "Pc", "Editor": "CD Projekt", "Formato": "Digital", "Precio": 40.000, "Stock": 12},
+    {"Titulo": "Let's go Pikachu", "Genero": "Aventura", "Anio":"2018", "Plataforma": "Switch", "Editor": "Nintendo", "Formato": "Físico", "Precio": 30.000, "Stock": 15},
+    {"Titulo": "Let's go Eevee", "Genero": "Aventura", "Anio":"2018", "Plataforma": "Switch", "Editor": "Nintendo", "Formato": "Físico", "Precio": 30.000, "Stock": 10},
+    {"Titulo": "Untitled Goose Game", "Genero": "Aventura", "Anio":"2019", "Plataforma": "Pc", "Editor": "House House", "Formato": "Digital", "Precio": 15.000, "Stock": 20},
+    {"Titulo": "God of War", "Genero": "Acción", "Anio":"2018", "Plataforma": "Pc", "Editor": "Santa Monica Studio", "Formato": "Digital", "Precio": 45.000, "Stock": 7},
+    {"Titulo": "Mario kart 8 Deluxe", "Genero": "Carreras", "Anio":"2017", "Plataforma": "Switch", "Editor": "Nintendo", "Formato": "Físico", "Precio": 40.000, "Stock": 9}, 
+] 
 
 # Carga de datos, ingreso y actualización de inventario se manejarán en este módulo.
 
@@ -31,23 +31,18 @@ def capturar_datos():
     Stock = int(input("Stock inicial: "))
 
     return {
-        "Titulo": Titulo,
-        "Genero": Genero,
-        "Anio": Anio,
-        "Plataforma": Plataforma,
-        "Editor": Editor,
-        "Formato": Formato,
-        "Precio": Precio,
-        "Stock": Stock
+        "Titulo": Titulo,"Genero": Genero,"Anio": Anio,
+        "Plataforma": Plataforma,"Editor": Editor,"Formato": Formato,
+        "Precio": Precio,"Stock": Stock
     }
 
 # Generar un nuevo ID para el videojuego
-nuevo_id = str(int(max(videojuegos_inventario.keys())) + 1).zfill(3)
+# nuevo_id = str(int(max(videojuegos_inventario.keys())) + 1).zfill(3)
 
 # Mostrar la existencia de inventario
-def mostrar_inventario(videojuegos_inventario):
+def mostrar_inventario(lista):
 
-    if not videojuegos_inventario:
+    if not lista:
         print("\n El inventario esta vacío actualmente.")
         return
     
@@ -55,19 +50,19 @@ def mostrar_inventario(videojuegos_inventario):
     print(f"{"INVENTARIO DE THE SAVE POINT":^60}") 
     print("="*60)
 
-    for Videojuego in videojuegos_inventario:
-    
-        print(f" Videojuego ID: {Videojuego}")
-        print(f"Genero: {videojuegos_inventario[Videojuego]['Genero']}")
+    for Videojuego in lista:
+        print(f"Titulo:{Videojuego["Titulo"]:<25} | Stock: {Videojuego["Stock"]}")
+        print(f"Genero: {Videojuego["Genero"]:<25} | Precio: ${Videojuego["Precio"]}")
+        print("-" * 60)
 
-def eliminar_videojuego(videojuegos_inventario):
+def eliminar_videojuego(lista):
     
     nombre_a_eliminar = input("\n Ingrese el nombre del videojuego a eliminar: ").strip().lower()
     encontrado = False
 
-    for videojuego_id, videojuego in videojuegos_inventario.items():
+    for videojuego_id in lista:
         if videojuego["Titulo"].lower() == nombre_a_eliminar:
-            del videojuegos_inventario[videojuego_id]
+            lista.remove(videojuego)
             print(f"El juego '{videojuego['Titulo']}' ha sido eliminado del inventario.")
             encontrado = True
             break
