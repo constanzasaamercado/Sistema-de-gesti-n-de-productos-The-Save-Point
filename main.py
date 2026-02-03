99c718f1551223d3fa8b16aef9f22f0870830ac8
Constanza Saa <constanza.saa.mercado@gmail.com>
2026-02-03 01:45:24 -0300

    Se crea el archivo menu.py y se mueven las funciones a este archivo, se modifcan varios codigos por errores producidos al mover las funciones, se corrigen los errores, se agrega la pregunta si desea eliminar el producto, se modifica el ingreso; se cambia el saludo, nombre de sistema y se coloca condicion para que se cierre el sistema al equivocarse 3 veces en el ingreso

--- main.py
+++ main.py
@@ -1,45 +1,42 @@
 # Importar funciones
 
-from modulos.gestion_datos import (
-    capturar_datos, 
-    eliminar_videojuego, 
-    modificar_videojuego,
-    login_sistema,
-    menu_visualizacion,
-    videojuegos_inventario
-)
+from modulos.menu import login_sistema, mostrar_bienvenida, capturar_datos, menu_visualizacion, modificar_videojuego, eliminar_videojuego
+from modulos.gestion_datos import videojuegos_inventario
 
-# Bienvenida al sistema de gestion de productos.
-
-print("¡BIENVENIDO A THE SAVE POINT!")
-nombre_usuario = input("Usuario: ")
-
-# Ingreso al sistema con usurio y contraseña
+# Ingreso al sistema con usuario y contraseña
 
 acceso_permitido, nombre_usuario = login_sistema()
 
 # Menu principal del sistema
+if acceso_permitido: 
+    mostrar_bienvenida(nombre_usuario) # Bienvenida al sistema de gestion de productos.
+    while True:
+        try:
+            print("\n--- MENU PRINCIPAL ---")
+            print("1. Ingresar datos | 2. Modificar datos | 3. Eliminar productos | 4. Visualización y Búsqueda de existencias | 5. Salir")
+            opcion = input("\nSeleccione una opción: ")
 
-while True:
-    print("\n--- MENU PRINCIPAL ---")
-    print("1. Ingresar datos | 2. Modificar datos| 3. Eliminar productos | 4. Visualización y Búsqueda de existencias | 5. Salir")
-    opcion = input("\nSeleccione una opción: ")
+            if opcion == "1":
+                capturar_datos()
+        
+            elif opcion == "2":
+                modificar_videojuego(videojuegos_inventario)
 
-    if opcion == "1":
-        nuevo = capturar_datos()
-        videojuegos_inventario.append(nuevo)
-        print(f"✅ {nuevo['Titulo']} guardado!")
-    
-    elif opcion == "2":
-       
-        modificar_videojuego(videojuegos_inventario)
+            elif opcion == "3":
+                eliminar_videojuego(videojuegos_inventario)
 
-    elif opcion == "3":
-        eliminar_videojuego(videojuegos_inventario)
-
-    if opcion == "4":
-        menu_visualizacion(videojuegos_inventario)
+            elif opcion == "4":
+                menu_visualizacion(videojuegos_inventario)
                
-    elif opcion == "5":
-        print(f"¡Hasta luego {nombre_usuario}!")
-        break
+            elif opcion == "5":
+                print(f"¡Hasta luego {nombre_usuario.upper()}!")
+                print("Saliendo del sistema...")
+                break
+            
+            else:
+                print("⚠️ Opción no válida. Intente de nuevo.")
+        
+        except KeyboardInterrupt:
+            print("\n\n❌ Operación cancelada por el usuario.")
+            print(f"¡Hasta luego {nombre_usuario.upper()}!")
+            break
